2b3d26880391f0d5a4e3ad8cf821fe4c4e338af6
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-01-12 19:11:22 -0300

    feat: get size boolean variable

---
+++ src/variables/get-size-boolean-variable.rs
@@ -0,0 +1,11 @@
+fn get_boolean_variable() -> usize {
+    let boolean_variable = true;
+
+    return std::mem::size_of_val(&boolean_variable);
+}
+
+fn main() {
+    let memory_size_boolean_variable = get_boolean_variable();
+
+    println!("boolean size = {}", memory_size_boolean_variable);
+}
