a4cb91bf080b47d609ddedb1fdb58f2db9c1f137
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-01-12 19:48:33 -0300

    feat: shadowing

--- src/specific-features/shadow-scope.rs
+++ src/specific-features/shadow-scope.rs
@@ -4,6 +4,9 @@ fn shadow_scope() {
     {
         let four_to_six = 456;
         println!("in shadow scope = {}", four_to_six);
+
+        let one_to_three = 321;
+        println!("in shadowing = {}", one_to_three);
     }
 
     println!("first = {}", one_to_three);
