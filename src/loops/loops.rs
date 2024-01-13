448536a8d05f205fbdf4f396a748d6470ea45822
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-01-12 23:23:27 -0300

    feat: loop

--- src/loops/loops.rs
+++ src/loops/loops.rs
@@ -1,5 +1,5 @@
-fn multiplication_table(number: u8) {
-    let mut counter: u8 = 0;
+fn multiplication_table(number: u32) {
+    let mut counter: u32 = 0;
     while counter < 10 {
         counter += 1;
         println!("{} X {} = {}", number, counter, number * counter);
@@ -7,7 +7,19 @@ fn multiplication_table(number: u8) {
 }
 
 fn main() {
+    // For in
     for i in 1..5 {
         multiplication_table(i);
+    }
+
+    // Infinite Loop
+    let mut counter: u32 = 1;
+    loop {
+        counter += 1;
+        multiplication_table(counter);
+
+        if counter == 100 * 1000 {
+            break;
+        }
     }
 }
