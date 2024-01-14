a1a9fc3f32a62922a66ccaf8f00a60ec4118b70f
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-01-14 09:06:57 -0300

    feat: ownersheap

--- src/memory/ownseship.rs
+++ src/memory/ownseship.rs
@@ -1,13 +1,15 @@
 fn ownsership() {
     let name = String::from("Leonardo");
 
-    steal(name);
+    let other_name = steal(name);
 
-    println!("{}", name);
+    println!("{}", other_name);
 }
 
-fn steal(string: String) {
+fn steal(string: String) -> String {
     println!("{}", string);
+
+    string
 }
 
 fn main() {
